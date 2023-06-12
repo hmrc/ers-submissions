f294538e2f901f4b4520e84d5dac5fee57189b52
MRBeresford <125281117+MRBeresford@users.noreply.github.com>
2023-06-12 11:50:48 +0100

    Ddce-4329 (#84)
    * DDCE-4329 Update TTL to 18 months and alow index to be overwritten
    
    * DDCE-4329 Configure replaceIndex as config variable

--- app/repositories/PresubmissionMongoRepository.scala
+++ app/repositories/PresubmissionMongoRepository.scala
@@ -49,7 +49,8 @@ class PresubmissionMongoRepository @Inject()(applicationConfig: ApplicationConfi
       IndexModel(ascending("createdAt"), indexOptions = IndexOptions().name("timeToLive")
           .expireAfter(applicationConfig.presubmissionCollectionTTL, TimeUnit.DAYS)
       )
-    )
+    ),
+    replaceIndexes = applicationConfig.presubmissionCollectionIndexReplace
   ) with Logging {
 
   private val objectIdKey: String = "_id"
