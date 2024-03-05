704dedd8bcf422f9f6335074d9891bcf6aa4896e
slavvco <13876870+slavvco@users.noreply.github.com>
2024-03-05 16:36:29 +0000

    DDCE-5338 | Added CSOP V5 config files.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,11 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "7.21.0"
-  private val alpakkaVersion = "3.0.4"
-  private val mongoVersion = "1.3.0"
+  private val bootstrapVersion = "7.23.0"
+  private val alpakkaVersion = "4.0.0"
+  private val mongoVersion = "1.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    play.sbt.PlayImport.ws,
     "uk.gov.hmrc"        %% "bootstrap-backend-play-28"          % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"                 % mongoVersion,
     "uk.gov.hmrc"        %% "domain"                             % "8.3.0-play-28",
@@ -18,12 +17,12 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28" % mongoVersion,
-    "org.scalatest"                %% "scalatest"               % "3.2.16",
+    "org.scalatest"                %% "scalatest"               % "3.2.18",
     "com.vladsch.flexmark"          % "flexmark-all"            % "0.64.8",
-    "org.scalatestplus"            %% "mockito-4-11"            % "3.2.16.0",
+    "org.scalatestplus"            %% "mockito-5-10"            % "3.2.18.0",
     "com.typesafe.akka"            %% "akka-testkit"            % "2.6.21",
-    "com.github.tomakehurst"        % "wiremock-standalone"     % "2.27.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.15.2"
+    "org.wiremock"                  % "wiremock-standalone"     % "3.4.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.16.1"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
