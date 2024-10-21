eb7f2d74f4f0c2aec3791f5117685a4c6a1a4690
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2024-10-21 14:11:59 +0100

    DDCE-5813 - Bootstrap-play upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "9.5.0"
   private val pekkoVersion = "1.0.2"
   private val mongoVersion = "1.8.0"
 
@@ -10,17 +10,17 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"           % mongoVersion,
     "uk.gov.hmrc"             %% "domain-play-30"               % "9.0.0",
     "org.apache.pekko"        %% "pekko-connectors-csv"         % pekkoVersion,
-    "org.typelevel"           %% "cats-core"                    % "2.10.0",
+    "org.typelevel"           %% "cats-core"                    % "2.12.0",
     "io.github.samueleresca"  %% "pekko-quartz-scheduler"       % "1.2.0-pekko-1.0.x"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.scalatest"                %% "scalatest"               % "3.2.18",
+    "org.scalatest"                %% "scalatest"               % "3.2.19",
     "com.vladsch.flexmark"          % "flexmark-all"            % "0.64.8",
-    "org.apache.pekko"             %% "pekko-testkit"           % pekkoVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.17.0",
+    "org.apache.pekko"             %% "pekko-testkit"           % "1.0.3",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.17.2",
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
