c4a9270cbf53e0912e54c5b526eb4daf398ef4d1
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-05-18 08:22:17 +0100

    DDCE-8603 - formatting comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,10 +11,11 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "domain-play-30"            % "11.0.0",
     "org.apache.pekko"        %% "pekko-connectors-csv"      % "1.0.2",
     "org.typelevel"           %% "cats-core"                 % "2.13.0",
+    // CVE-2026-27830: pekko-quartz-scheduler -> quartz -> c3p0 pulls in com.mchange:c3p0. This should be removed when
+    // pekko deps >= 1.2.x/1.3.x & scheduler upgrades to Quartz >= 2.5.0 which removes the c3p0 dependency
     ("io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.2-pekko-1.0.x")
       .exclude("com.mchange", "c3p0")
-      .exclude("com.mchange", "mchange-commons-java")     // CVE-2026-27830: pekko-quartz-scheduler -> quartz -> c3p0 pulls in com.mchange:c3p0. This should be removed when:
-                                                          // playframework=3.1.x ; pekko deps >= 1.2.x/1.3.x
+      .exclude("com.mchange", "mchange-commons-java")
   )
 
   private val test: Seq[ModuleID] = Seq(
