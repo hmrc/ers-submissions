f4676ba6af439cad41260a4924d4bf260eca9163
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-05-19 13:52:48 +0100

    DDCE-8603 - updating dependencies (#152)
    * DDCE-8603 - updating dependencies
    
    * DDCE-8603 - reverting the conf changes
    
    * DDCE-8603 - adding comment
    
    * DDCE-8603 - formatting comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,16 @@ object AppDependencies {
   private val mongoVersion     = "2.12.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % mongoVersion,
-    "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0",
-    "org.apache.pekko"       %% "pekko-connectors-csv"      % "1.0.2",
-    "org.typelevel"          %% "cats-core"                 % "2.13.0",
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.2-pekko-1.0.x"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"        % mongoVersion,
+    "uk.gov.hmrc"             %% "domain-play-30"            % "11.0.0",
+    "org.apache.pekko"        %% "pekko-connectors-csv"      % "1.0.2",
+    "org.typelevel"           %% "cats-core"                 % "2.13.0",
+    // CVE-2026-27830: pekko-quartz-scheduler -> quartz -> c3p0 pulls in com.mchange:c3p0. This should be removed when
+    // pekko deps >= 1.2.x/1.3.x & scheduler upgrades to Quartz >= 2.5.0 which removes the c3p0 dependency
+    ("io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.2-pekko-1.0.x")
+      .exclude("com.mchange", "c3p0")
+      .exclude("com.mchange", "mchange-commons-java")
   )
 
   private val test: Seq[ModuleID] = Seq(
